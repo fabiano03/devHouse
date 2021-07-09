eb67f5fb38675b94ddfbe80ea026efb17b40e56e
Fabiano Lee <fabianoyslee@gmail.com>
2021-07-08 22:03:32 -0300

    inclusão página blog

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
 
 app.get('/', (req,res)=> {
-    res.send("Novo servidor")
+    res.sendFile(__dirname + "/views/home.html")
 });
 
 app.get('/manutencao', (req,res)=> {
     res.sendFile(__dirname + "/views/manutencao.html")
 });
+
+app.get('/home', (req,res)=> {
+    res.sendFile(__dirname + "/views/home.html")
+});
+
+app.get('/blog', (req,res)=> {
+    res.sendFile(__dirname + "/views/blog.html")
+});
+
 app.listen(3000);
